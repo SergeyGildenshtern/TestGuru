dc4df39a59f2fdacafd47137dc5cc10f0502712c
Sergey Gildenshtern <gildenshtern001@gmail.com>
2021-12-07 22:03:23 +0700

    Lesson 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_141118) do
+ActiveRecord::Schema.define(version: 2021_12_07_145727) do
 
   create_table "answers", force: :cascade do |t|
     t.text "text", null: false
-    t.boolean "correct", default: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -47,10 +47,12 @@ ActiveRecord::Schema.define(version: 2021_12_07_141118) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", default: 0
+    t.integer "level", default: 0, null: false
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -67,4 +69,5 @@ ActiveRecord::Schema.define(version: 2021_12_07_141118) do
   add_foreign_key "results", "tests"
   add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
