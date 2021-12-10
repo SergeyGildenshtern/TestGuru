42871e16b0014f71905a069a99bf22911e1b79e2
Sergey Gildenshtern <gildenshtern001@gmail.com>
2021-12-10 16:03:07 +0700

    Fix models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.category_tests(category)
-    Test.joins('LEFT JOIN categories ON categories.id = tests.category_id AND categories.title = ?',
+    Test.joins('INNER JOIN categories ON categories.id = tests.category_id AND categories.title = ?',
                category).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def user_tests(level)
-    Test.joins('LEFT JOIN results ON tests.id = results.test_id AND results.user_id = ?', id).where(level: level)
+    Test.joins('INNER JOIN results ON tests.id = results.test_id AND results.user_id = ?', id).where(level: level)
   end
 end
