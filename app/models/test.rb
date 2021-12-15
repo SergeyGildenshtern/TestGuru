e77825f325a55c8f750039b6b5c6d2e4619c0f9b
Sergey Gildenshtern <gildenshtern001@gmail.com>
2021-12-15 20:37:06 +0700

    Lesson 5 - Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_and_belongs_to_many :users
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   def self.category_tests(category)
     Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
           .where(categories: { title: category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+  has_many :tests
+
   def user_tests(level)
     Test.joins('INNER JOIN results ON tests.id = results.test_id').where(results: { user_id: id }, tests: { level: level })
   end
