876bcf5e254ef01800b14d6893cf5cb7af80963a
Sergey Gildenshtern <gildenshtern001@gmail.com>
2021-12-16 21:02:43 +0700

    Fix user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tests, dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   has_many :results, dependent: :destroy
   has_many :tests, through: :results, dependent: :destroy
 
