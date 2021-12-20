a56d98b3698e15d111d433d8d4de1c9965b32843
Sergey Gildenshtern <gildenshtern001@gmail.com>
2021-12-20 21:35:52 +0700

    Lesson 6 - Add scope methods and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,16 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :text, presence: true
+  validate :validate_count_answers, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_count_answers
+    if question.answers.size > 4
+      errors.add(:base, :count_answers, message: 'count of answers to the question should be from 1 to 4')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results
 
+  validates :email, presence: true
+
   def user_tests(level)
     Test.joins(:results).where(results: { user_id: id }, tests: { level: level })
   end
