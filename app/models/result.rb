80077752fdacac9c1a33885e28e12cdcb8526366
Sergey Gildenshtern <gildenshtern001@gmail.com>
2022-01-13 14:32:31 +0700

    Fix commit

--- app/models/result.rb
+++ app/models/result.rb
@@ -32,7 +32,7 @@ class Result < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
 
   def correct_answers
