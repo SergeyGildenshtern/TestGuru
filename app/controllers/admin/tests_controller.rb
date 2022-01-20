331cb0200d5f9e872d926abb5eed1fd767eaeed1
Sergey Gildenshtern <gildenshtern001@gmail.com>
2022-01-20 21:34:08 +0700

    Lesson 11 - Add Devise gem and admin panel

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,57 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show edit update destroy start]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author = current_user
+
+    if @test.save
+      redirect_to [:admin, @test]
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.start_test(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+end
